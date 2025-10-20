faa706f0f890e9188b36deb4251190d12519d020
renan-senai <renan.andrade@edu.sc.senai.br>
2025-10-20 13:04:06 -0300

    exercicio 3

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Produto } from '../shared/models/Produto';
+import { Projeto } from '../shared/models/Projeto';
 
 @Component({
   selector: 'app-root',
@@ -10,40 +11,44 @@ import { Produto } from '../shared/models/Produto';
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements OnInit {
+  ngOnInit(): void {
+    this.loadPrioridades();
+  }
 
-  protected produtos: Produto[] = [
-    new Produto("Camiseta Algodão", "Malharia Sol", 150, 39.90),
-    new Produto("Notebook Gamer", "Mega Informática", 12, 3499.00),
-    new Produto("Fone de Ouvido Bluetooth", "TechSound", 40, 129.99),
-    new Produto("Copo Térmico", "Acessórios Vip", 85, 59.90),
-    new Produto("Tênis Esportivo", "Runner Shoes", 37, 219.50),
-    new Produto("Jaqueta Corta Vento", "Moda Ativa", 24, 179.90),
-    new Produto("Relógio Digital", "TimeLux", 16, 289.00),
-    new Produto("Garrafa Squeeze", "FIT Acessórios", 58, 24.90),
-    new Produto("Headset Gamer", "TechSound", 13, 359.00),
-    new Produto("Livro JavaScript Básico", "Livraria Central", 33, 45.00)
+  projetos: Projeto[] = [
+    new Projeto('Automação da Esteira', 'Concluído', 'Alta'),
+    new Projeto('Layout do Lab', 'Em andamento', 'Média'),
+    new Projeto('Revisão Elétrica', 'Pendente', 'Alta'),
+    new Projeto('Redução de Desperdício', 'Concluído', 'Baixa'),
+    new Projeto('Implantação de ERP', 'Em andamento', 'Alta'),
+    new Projeto('Padronização de Processo', 'Pendente', 'Média'),
+    new Projeto('Treinamento de Segurança', 'Concluído', 'Média'),
+    new Projeto('Monitoramento de Energia', 'Em andamento', 'Baixa'),
+    new Projeto('Modernização do Almoxarifado', 'Pendente', 'Alta'),
+    new Projeto('Gestão de Inventário', 'Em andamento', 'Média'),
+    new Projeto('Otimização de Setup', 'Concluído', 'Alta'),
+    new Projeto('Controle de Qualidade', 'Pendente', 'Baixa'),
+    new Projeto('Reforma do Escritório', 'Em andamento', 'Baixa'),
+    new Projeto('Atualização de Software', 'Concluído', 'Média')
   ];
 
-  protected opcao = '0';
+  protected prioridades: {prioridade: string, qtd: number}[] = []
+  protected statusCombo = ['Selecione uma opção', 'Concluído', 'Em andamento', 'Pendente']
+  protected statusSelecionado = 0;
 
-  reordenarLista() {
-    switch (this.opcao) {
-      case '1':
-        this.produtos = this.produtos.sort((p1, p2) => p1.nome.localeCompare(p2.nome));
-        break;
-      case '2':
-        this.produtos = this.produtos.sort((p1, p2) => p1.fornecedor.localeCompare(p2.fornecedor));
-        break;
-      case '3':
-        this.produtos = this.produtos.sort((p1, p2) => p1.estoque - p2.estoque);
-        break;
-      case '4':
-        this.produtos = this.produtos.sort((p1, p2) => p1.preco - p2.preco);
-        break;
-      default:
-        break;
+  loadPrioridades() {
+    this.prioridades = [];
+    this.prioridades.push({ prioridade: 'Alta', qtd: this.listaFiltrada.filter(e => e.prioridade == 'Alta').length });
+    this.prioridades.push({ prioridade: 'Média', qtd: this.listaFiltrada.filter(e => e.prioridade == 'Média').length });
+    this.prioridades.push({ prioridade: 'Baixa', qtd: this.listaFiltrada.filter(e => e.prioridade == 'Baixa').length });
+  }
+
+  get listaFiltrada() {
+    if (this.statusSelecionado == 0) {
+      return this.projetos;
     }
+    return this.projetos.filter(e => e.status == this.statusCombo[this.statusSelecionado])
   }
 
 }
